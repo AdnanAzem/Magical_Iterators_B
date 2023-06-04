9ca1efb96add59f046489fac975007f499c0ac8c
AdnanAzem <adnan.azem.250@gmail.com>
2023-06-04 22:30:24 +0300

    change to be inherited classes

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -26,6 +26,12 @@ namespace ariel{
     void MagicalContainer::addElement(int element) {
         elements.push_back(element);
         std::sort(elements.begin(), elements.end());
+
+        primeElements.clear();
+        for(size_t i = 0; i < elements.size(); i++)
+        if(isPrime(elements.at(i))){
+            primeElements.push_back(&elements.at(i));
+        }
     }
 
     // Method to remove an element from the container
@@ -40,7 +46,7 @@ namespace ariel{
     }
 
     // Method to retrieve the size of the container
-    int MagicalContainer::size() const {
+    size_t MagicalContainer::size() const {
         return elements.size();
     }
 
@@ -50,74 +56,53 @@ namespace ariel{
 
     //=========================== AscendingIterator Class ===========================
 
-    MagicalContainer::AscendingIterator::AscendingIterator( MagicalContainer &cont, int curr)
-        : container(cont), currentIndex(curr) {}
+    MagicalContainer::AscendingIterator::AscendingIterator( MagicalContainer &cont, size_t index): Iterator(cont,index){}
 
     // Default constructor
-    MagicalContainer::AscendingIterator::AscendingIterator( MagicalContainer& magicalContainer) : container(magicalContainer), currentIndex(0) {}
+    MagicalContainer::AscendingIterator::AscendingIterator( MagicalContainer& magicalContainer): Iterator(magicalContainer){} //: container(magicalContainer), currentIndex(0) {}
 
     // Copy constructor
-    MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator& other) : container(other.container), currentIndex(other.currentIndex) {}
+    MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator& other):Iterator(other.getContainer(),other.getIndex()){}// : container(other.container), currentIndex(other.currentIndex) {}
 
     // Destructor
     MagicalContainer::AscendingIterator::~AscendingIterator() {}
 
     // Assignment operator
     MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator=(const AscendingIterator& other) {
-        if(&container != &other.container){ // throw error if we try to assign different containers
+
+        if(&getContainer() != &other.getContainer()){ // throw error if we try to assign different containers
             throw runtime_error("Error: Iterators cannot be assigned if they belong to different containers.");
         }
         if (this != &other) {
-            container = other.container;
-            currentIndex = other.currentIndex;
+            setContainer(other.getContainer());
+            setIndex(other.getIndex());
         }
         return *this;
     }
 
-    // Equality comparison
-    bool MagicalContainer::AscendingIterator::operator==(const AscendingIterator& other) const {
-        return &container == &other.container && currentIndex == other.currentIndex;
-    }
-
-    // Inequality comparison
-    bool MagicalContainer::AscendingIterator::operator!=(const AscendingIterator& other) const {
-        return !(*this == other);
-    }
-
-    // Greater than comparison
-    bool MagicalContainer::AscendingIterator::operator>(const AscendingIterator& other) const {
-        return currentIndex > other.currentIndex;
-    }
-
-    // Less than comparison
-    bool MagicalContainer::AscendingIterator::operator<(const AscendingIterator& other) const {
-        return currentIndex < other.currentIndex;
-    }
-
     // Dereference operator
-    int MagicalContainer::AscendingIterator::operator*() const {
-        return container.getElements().at((std::vector<int>::size_type)currentIndex);
-        
+    int MagicalContainer::AscendingIterator::operator*() const {        
+        return getContainer().getElements().at((std::vector<int>::size_type)getIndex());     
     }
 
     // Pre-increment operator
     MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator++() {
-        if(currentIndex >= container.getElements().size() || *this == end()){ // throw error when try to increase while we at the end
+        if(getIndex() >= getContainer().getElements().size() || *this == end()){ // throw error when try to increase while we at the end
             throw runtime_error("Error: Iterator out of bounds!!!");
         }
-        currentIndex++;
+        increase(1); // increase currentIndex by 1.
         return *this;
     }
 
     // Method to get an AscendingIterator pointing to the first element
     MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin() const {
-        return AscendingIterator(container,0);
+        return AscendingIterator(getContainer(),0);
         
     }
 
     // Method to get an AscendingIterator pointing one position past the last element
     MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end() const {
-        return AscendingIterator(container,container.size());
+        return AscendingIterator(getContainer(),getContainer().size());
     }
     //=========================== End AscendingIterator Class ===========================
 
@@ -125,79 +110,56 @@ namespace ariel{
     //=========================== SideCrossIterator Class ===========================
 
         
-    MagicalContainer::SideCrossIterator::SideCrossIterator( MagicalContainer &cont, int index):
-    container(cont),currentIndex(index){}
+    MagicalContainer::SideCrossIterator::SideCrossIterator( MagicalContainer &cont, size_t index): Iterator(cont,index){}
 
-    MagicalContainer::SideCrossIterator::SideCrossIterator(MagicalContainer& magicalContainer) 
-    :container(magicalContainer), currentIndex(0){}
+    MagicalContainer::SideCrossIterator::SideCrossIterator(MagicalContainer& magicalContainer) : Iterator(magicalContainer,0){}
 
     // Copy constructor
-    MagicalContainer::SideCrossIterator::SideCrossIterator(const SideCrossIterator& other) 
-    : container(other.container), currentIndex(other.currentIndex){}
+    MagicalContainer::SideCrossIterator::SideCrossIterator(const SideCrossIterator& other) : Iterator(other.getContainer(), other.getIndex()){}
         
-
     // Destructor
     MagicalContainer::SideCrossIterator::~SideCrossIterator() {}
 
     // Assignment operator
     MagicalContainer::SideCrossIterator& MagicalContainer::SideCrossIterator::operator=(const SideCrossIterator& other) {
-        if(&container != &other.container){ // throw error if we try to assign different containers
+        if(&getContainer() != &other.getContainer()){ // throw error if we try to assign different containers
             throw runtime_error("Error: Iterators cannot be assigned if they belong to different containers.");
         }
         if (this != &other) {
-            container = other.container;
-            currentIndex = other.currentIndex;
+            setContainer(other.getContainer());
+            setIndex(other.getIndex());
+
         }
         return *this;
     }
 
-    // Equality comparison
-    bool MagicalContainer::SideCrossIterator::operator==(const SideCrossIterator& other) const {
-        return currentIndex == other.currentIndex && &container == &other.container ;
-    }
-
-    // Inequality comparison
-    bool MagicalContainer::SideCrossIterator::operator!=(const SideCrossIterator& other) const {
-        return !(*this == other);
-    }
-
-    // Greater than comparison
-    bool MagicalContainer::SideCrossIterator::operator>(const SideCrossIterator& other) const {
-        return currentIndex > other.currentIndex;
-    }
-
-    // Less than comparison
-    bool MagicalContainer::SideCrossIterator::operator<(const SideCrossIterator& other) const {
-        return currentIndex < other.currentIndex;  
-    }
-
     // Dereference operator
     int MagicalContainer::SideCrossIterator::operator*() const { 
-        if (!(currentIndex % 2)) { 
-            return container.elements.at((std::vector<int>::size_type)(currentIndex / 2));
+        if (!(getIndex() % 2)) { 
+            return getContainer().elements.at((std::vector<int>::size_type)(getIndex() / 2));
         }
         else {
-            return container.elements.at((std::vector<int>::size_type)(container.size() - 1 - (currentIndex - 1 ) / 2));
+            return getContainer().elements.at((std::vector<int>::size_type)(getContainer().size() - 1 - (getIndex() - 1 ) / 2));
         }
     }
 
     // Pre-increment operator
     MagicalContainer::SideCrossIterator& MagicalContainer::SideCrossIterator::operator++() {
-        if(currentIndex >= container.getElements().size() || *this == end()){ // throw error when try to increase while we at the end
+        if(getIndex() >= getContainer().getElements().size() || *this == end()){ // throw error when try to increase while we at the end
             throw runtime_error("Error: Iterator out of bounds!!!");
         }
-        currentIndex++;
+        increase(1);
         return *this;
     }
 
     // Method to get a SideCrossIterator pointing to the first element
     MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::begin() const {
-        return SideCrossIterator(container,0); 
+        return SideCrossIterator(getContainer(),0); 
     }
 
     // Method to get a SideCrossIterator pointing one position past the last element
     MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::end() const {
-        return SideCrossIterator(container,container.size());
+        return SideCrossIterator(getContainer(),getContainer().size());
     }
 
     //=========================== End SideCrossIterator Class ===========================
@@ -205,83 +167,60 @@ namespace ariel{
 
     //=========================== PrimeIterator Class ===========================
 
-    MagicalContainer::PrimeIterator::PrimeIterator( MagicalContainer &cont, int curr)
-        : container(cont), currentIndex(curr) {}
+    MagicalContainer::PrimeIterator::PrimeIterator( MagicalContainer &cont, size_t index) : Iterator(cont,index){}
 
     // Default constructor
-    MagicalContainer::PrimeIterator::PrimeIterator( MagicalContainer& magicalContainer) : container(magicalContainer), currentIndex(0) {
-        // Skip non-prime numbers at the beginning
-        while (currentIndex < container.size() && !isPrime(container.getElements().at((std::vector<int>::size_type)currentIndex))) {
-            currentIndex++;
-        }
+    MagicalContainer::PrimeIterator::PrimeIterator( MagicalContainer& magicalContainer) : Iterator(magicalContainer, 0){
     }
 
     // Copy constructor
-    MagicalContainer::PrimeIterator::PrimeIterator(const PrimeIterator& other) : container(other.container), currentIndex(other.currentIndex) {}
+    MagicalContainer::PrimeIterator::PrimeIterator(const PrimeIterator& other) : Iterator(other.getContainer(), other.getIndex()){}
+    
 
     // Destructor
     MagicalContainer::PrimeIterator::~PrimeIterator() {}
 
     // Assignment operator
     MagicalContainer::PrimeIterator& MagicalContainer::PrimeIterator::operator=(const PrimeIterator& other) {
-        if(&container != &other.container){ // throw error if we try to assign different containers
+        if(&getContainer() != &other.getContainer() ){ // throw error if we try to assign different containers
             throw runtime_error("Error: Iterators cannot be assigned if they belong to different containers.");
         }
         if (this != &other) {
-            container = other.container;
-            currentIndex = other.currentIndex;
+            setContainer(other.getContainer()) ;
+            setIndex(other.getIndex());
         }
         return *this;
     }
 
-    // Equality comparison
-    bool MagicalContainer::PrimeIterator::operator==(const PrimeIterator& other) const {
-        return &container == &other.container && currentIndex == other.currentIndex;
-    }
-
-    // Inequality comparison
-    bool MagicalContainer::PrimeIterator::operator!=(const PrimeIterator& other) const {
-        return !(*this == other);
-    }
-
-    // Greater than comparison
-    bool MagicalContainer::PrimeIterator::operator>(const PrimeIterator& other) const {
-        return currentIndex > other.currentIndex;
-    }
-
-    // Less than comparison
-    bool MagicalContainer::PrimeIterator::operator<(const PrimeIterator& other) const {
-        return currentIndex < other.currentIndex;
-    }
-
     // Dereference operator
     int MagicalContainer::PrimeIterator::operator*() const {
-        return container.getElements().at((std::vector<int>::size_type)currentIndex);
+        return *getContainer().getPrimeElements().at((std::vector<int>::size_type)getIndex());
     }
 
     // Pre-increment operator
     MagicalContainer::PrimeIterator& MagicalContainer::PrimeIterator::operator++() {
-        if(currentIndex >= container.getElements().size() || *this == end()){// throw error when try to increase while we at the end
+        if(getIndex() >= getContainer().getPrimeElements().size() || *this == end()){// throw error when try to increase while we at the end
             throw runtime_error("Error: Iterator out of bounds!!!");
         }
-        currentIndex++;
-        if (currentIndex < container.size() && !isPrime(container.getElements().at((std::vector<int>::size_type)currentIndex))) {
-            currentIndex++;
-        }
+        increase(1);
         return *this;
     }
 
     // Method to get a PrimeIterator pointing to the first prime element
     MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::begin() const {
-        return PrimeIterator(container,0);
+        return PrimeIterator(getContainer(),0);
     }
 
     // Method to get a PrimeIterator pointing one position past the last prime element
     MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::end() const {
-        return PrimeIterator(container,container.size());
+        return PrimeIterator(getContainer(),getContainer().getPrimeElements().size());
     }
 
     //=========================== End PrimeIterator Class ===========================
+
+    MagicalContainer::Iterator::~Iterator()
+    {
+    }
 
     //=========================== End MagicalContainer Class ===========================
 }
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -6,25 +6,53 @@ namespace ariel{
 
     class MagicalContainer{
         private:
-            vector<int> elements;
+            vector <int> elements;
+            vector <int*> primeElements;
+            
 
         public:
             MagicalContainer(); // Default constructor
             void addElement(int); // Adding element to the container
-            int size()const; // Return the size of the container
+            size_t size() const; // Return the size of the container
             void removeElement(int); // Removing element from the container
             vector<int> getElements() const; // return the container
+            vector<int*> getPrimeElements() const { return primeElements;}
+
+            class Iterator{
+                private:
+                    MagicalContainer& container;
+                    size_t currentIndex;
+
+                public:
+
+                    Iterator() = delete; 
+                    Iterator(Iterator&&) = delete;
+                    Iterator& operator=(Iterator&&) = delete;  
+                    Iterator& operator=(const Iterator& other) = delete;  
+                                     
+                    Iterator(MagicalContainer& magicalContainer): container(magicalContainer), currentIndex(0){}
+                    Iterator(MagicalContainer& magicalContainer, size_t index): container(magicalContainer), currentIndex(index){}
+                    Iterator(const Iterator& other):container(other.container), currentIndex(other.currentIndex){}
+
+                    void setContainer(MagicalContainer& cont){ container = cont;}
+                    void setIndex(size_t num){ currentIndex = num;}
+                    void increase(size_t num){ currentIndex += num;}
+                    MagicalContainer& getContainer() const{return container;}
+                    size_t getIndex() const{return currentIndex;}
+
+                    virtual bool operator==(const Iterator& other) const{return &container == &other.container && currentIndex == other.currentIndex;}// Equality comparison  
+                    virtual bool operator!=(const Iterator& other) const{return !(*this == other);}// Inequality comparison   
+                    virtual bool operator>(const Iterator& other) const{return currentIndex > other.currentIndex;}// Greater than comparison  
+                    virtual bool operator<(const Iterator& other) const{return currentIndex < other.currentIndex;}// Less than comparison  
+                    virtual ~Iterator();
+
+            };
 
 
-        class AscendingIterator{
-            private:
-                MagicalContainer& container;
-                int currentIndex;
+        class AscendingIterator: public Iterator{
 
             public:
-
-
-            AscendingIterator( MagicalContainer &, int);
+            AscendingIterator( MagicalContainer &, size_t);
             AscendingIterator( MagicalContainer& magicalContainer);
             AscendingIterator(const AscendingIterator& other);// Copy constructor      
 
@@ -32,61 +60,44 @@ namespace ariel{
             AscendingIterator(AscendingIterator&&) = delete;
             AscendingIterator& operator=(AscendingIterator&&) = delete;
 
-            ~AscendingIterator() ;// Destructor    
+            ~AscendingIterator() override;// Destructor    
             AscendingIterator& operator=(const AscendingIterator& other);// Assignment operator   
-            bool operator==(const AscendingIterator& other) const;// Equality comparison  
-            bool operator!=(const AscendingIterator& other) const;// Inequality comparison   
-            bool operator>(const AscendingIterator& other) const;// Greater than comparison  
-            bool operator<(const AscendingIterator& other) const;// Less than comparison  
             int operator*() const ;// Dereference operator   
             AscendingIterator& operator++(); // Pre-increment operator
             AscendingIterator begin()const;
             AscendingIterator end()const;
         };
 
-        class SideCrossIterator{
-            private:
-                MagicalContainer& container;
-                int currentIndex;
+        class SideCrossIterator : public Iterator{
 
             public:
                 SideCrossIterator() = delete; 
                 SideCrossIterator(SideCrossIterator&&) = delete;
                 SideCrossIterator& operator=(SideCrossIterator&&) = delete;
 
-                SideCrossIterator( MagicalContainer &, int);
+                SideCrossIterator( MagicalContainer &, size_t);
                 SideCrossIterator( MagicalContainer& magicalContainer) ;                
                 SideCrossIterator(const SideCrossIterator& other) ;// Copy constructor       
-                ~SideCrossIterator() ;// Destructor              
-                SideCrossIterator& operator=(const SideCrossIterator& other);// Assignment operator       
-                bool operator==(const SideCrossIterator& other) const ;// Equality comparison               
-                bool operator!=(const SideCrossIterator& other) const ;// Inequality comparison               
-                bool operator>(const SideCrossIterator& other) const ;// Greater than comparison               
-                bool operator<(const SideCrossIterator& other)const;// Less than comparison               
+                ~SideCrossIterator() override;// Destructor              
+                SideCrossIterator& operator=(const SideCrossIterator& other);// Assignment operator                 
                 int operator*() const ; // Dereference operator                
                 SideCrossIterator& operator++() ; // Pre-increment operator
                 SideCrossIterator begin()const;
                 SideCrossIterator end()const;
         };
 
-        class PrimeIterator{
-            private:
-                MagicalContainer& container;
-                int currentIndex;
+        class PrimeIterator : public Iterator{
+
             public:
                 PrimeIterator() = delete; 
                 PrimeIterator(PrimeIterator&&) = delete;
                 PrimeIterator& operator=(PrimeIterator&&) = delete;
 
-                PrimeIterator( MagicalContainer &, int);
+                PrimeIterator( MagicalContainer &, size_t);
                 PrimeIterator( MagicalContainer& magicalContainer);
                 PrimeIterator(const PrimeIterator& other);// Copy constructor
-                ~PrimeIterator();// Destructor 
+                ~PrimeIterator() override;// Destructor 
                 PrimeIterator& operator=(const PrimeIterator& other);// Assignment operator
-                bool operator==(const PrimeIterator& other) const ;// Equality comparison 
-                bool operator!=(const PrimeIterator& other) const ;// Inequality comparison
-                bool operator>(const PrimeIterator& other) const ;// Greater than comparison   
-                bool operator<(const PrimeIterator& other) const ;// Less than comparison  
                 int operator*() const ;// Dereference operator
                 PrimeIterator& operator++() ;// Pre-increment operator
                 PrimeIterator begin()const;
